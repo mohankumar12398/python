5280e9394e7fd0f471d1830420121abeec5bf4fa
mohankumar12398 <99006423+mohankumar12398@users.noreply.github.com>
2022-02-08 13:46:34 +0530

    Update main.py
    change the coding

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import datetime
+x=datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.strftime("%A"))
+print(x.strftime("%I"))
+print(x.strftime("%p"))
+print(x.strftime("%c"))
+#print(x.strftime("%z"))
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    
